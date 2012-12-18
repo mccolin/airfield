bd87070f80b0871fcda5d8ab9c633606de3816e9
Colin McCloskey <colin.mccloskey@gmail.com>
2012-12-17 21:52:28 -0500

    rails new airfield

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airfield::Application.config.secret_token = '8cef5f1b0c6f911242b2dabcb24a05509c6ef83aeeff3f9a0cdda2c2e89c98bbccf7839ef0755615e975bebca39e748ff053b605fa7203242cb3a55a8d4c06ac'
