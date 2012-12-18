57ffb5cab17380f1e65bdb8377b0690ad570bc6f
Colin McCloskey <colin.mccloskey@gmail.com>
2012-12-18 03:49:00 -0500

    autolink in markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   # Render a chunk of Markdown content:
   def render_markdown(src)
     @renderer ||= Redcarpet::Render::HTML.new
-    @marker ||= Redcarpet::Markdown.new(@renderer)
+    @marker ||= Redcarpet::Markdown.new(@renderer, :autolink=>true)
     @marker.render(src).html_safe
   end
 
