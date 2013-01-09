879fc29f9be034677eaf1fabcc29fe586df3cc57
Colin McCloskey <colin.mccloskey@gmail.com>
2013-01-09 00:08:13 -0500

    basic content insertion for posts

--- app/assets/javascripts/editables.js
+++ app/assets/javascripts/editables.js
@@ -9,16 +9,18 @@ $(function(){
     e.preventDefault();
     var $toggle = $(this);
     if ($toggle.hasClass("highlight"))
-      $toggle.removeClass("highlight").html( $toggle.html().replace("Editing...","Edit") ).trigger("deactivateEdit");
+      $toggle.trigger("deactivateEdit");
     else
-      $toggle.addClass("highlight").html( $toggle.html().replace("Edit","Editing...") ).trigger("activateEdit");
+      $toggle.trigger("activateEdit");
   });
+
 
   /** Starter: Begin editable mode on this page: */
   $("#edit-mode-toggle").on("activateEdit", function(e){
     e.preventDefault();
-    var $toggle = $(this);
-    $toggle.addClass("highlight");
+    $(this).addClass("highlight").html( $(this).html().replace("Edit","Editing...") );
+
+    // Make editable fields active:
     $(".editable").addClass("activated").hallo({
       editable: true,
       toolbar: "halloToolbarContextual",    // halloToolbarFixed
@@ -34,15 +36,24 @@ $(function(){
       },
     });
     console.log( $(".editable").length + " editable regions enabled on page.");
+
+    // Make insertable/create fields active:
+    $(".insertable").addClass("activated");
   });
+
 
   /** Stopper: Stop Editing */
   $("#edit-mode-toggle").on("deactivateEdit", function(e){
     e.preventDefault();
-    var $toggle = $(this);
-    $toggle.removeClass("highlight");
+    $(this).removeClass("highlight").html( $(this).html().replace("Editing...","Edit") );
+
+    // Deactivate editable fields on the page:
     $(".editable").removeClass("activated").hallo({editable:false});
+
+    // Deactivate insertable/create fields:
+    $(".insertable").removeClass("activated");
   });
+
 
   /** Markdown Conversion: Support HTML->Markdown processing: */
   var markdownize = function(content) {
@@ -52,8 +63,10 @@ $(function(){
     return toMarkdown(html);
   };
 
+
   /** Activation: When user focues on field, take action: */
   $(".editable").on("halloactivated", function(e, data){});
+
 
   /** Deactivation: When user unfocues from field, send updates to server: */
   $(".editable").on("hallodeactivated", function(e, data){
@@ -80,6 +93,42 @@ $(function(){
       });
       console.log("Server request for content update sent to server.");
     }
+  });
+
+
+  /** Insertable: Save new content to server: */
+  $(".insertable .save-button").click(function(e){
+    e.preventDefault();
+    var $saveButton = $(this);
+    var $insertable = $saveButton.parents(".insertable").first();
+
+    var contentData = new Array();
+    $insertable.find(".field").each(function(idx, el){
+      var $field = $(this);
+      contentData.push({key: $field.attr("data-content-key"), value: markdownize($field.html()) });
+    });
+
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: $saveButton.prop("href"),
+      data: {
+        type: $insertable.attr("data-content-type"),
+        content: contentData
+      },
+      success: function(data, status, xhr){
+        console.log("Server response returned to script. Inserting new content, now.");
+        if (data.success && data.rendered) {
+          $insertable.hide().after(data.rendered);
+        } else {
+          $insertable.hide().after("<span class=\"error parse-error ui-state-error\">Failed to save new content</span>");
+        }
+      },
+      error: function(xhr, status, message){
+        $insertable.hide().after("<span class=\"error parse-error ui-state-error\">Failed to save new content: "+message+"</span>");
+      }
+    });
+    console.log("Server request for content creation sent to server.");
   });
 
 });
--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -5,6 +5,33 @@ class ContentController < ApplicationController
 
   # Create content
   def create
+    klass = params[:type].camelcase.constantize
+    obj = klass.new()
+    params[:content].each do |idx, data|
+      content_key =  data[:key]
+      content_value = data[:value]
+
+      logger.debug "#{content_key} => #{content_value}"
+
+      if content_key && content_key == "name"
+        obj.name = content_value
+      elsif content_key && content_key != "content"
+        # Strongly-keyed content with regions (like pages):
+        obj.content[content_key] = content_value
+      else
+        # Single text content (like Posts):
+        obj.content = content_value
+      end
+    end
+
+    raise Exception.new("Funky exception")
+
+    obj.author = current_user if obj.respond_to?(:author=)
+    obj.save
+
+    partial_name = klass.to_s.downcase.underscore
+
+    render :json=>{success:true, object:obj, content:obj.content, rendered:render_to_string(:partial=>"site/#{partial_name}", :collection=>[obj], :locals=>{new_record:true})}
   end
 
   # Update content
