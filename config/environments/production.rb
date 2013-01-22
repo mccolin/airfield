1b8173a71fbf37e7d516f2d708ac93fc38222cb5
Colin McCloskey <colin.mccloskey@gmail.com>
2013-01-22 15:18:43 -0500

    attempt regex precompile file list to resolve heroku deploy issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Airfield::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( *.js *.css )
+  #config.assets.precompile += %w( *.js *.css )
+  config.assets.precompile += [/^[a-z0-9]\w+.(css|js)$/]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
